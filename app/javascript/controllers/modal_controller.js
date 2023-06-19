cdbf8f5fc953fef88f2a1bed6af3c26181e6bd37
AlexCosmas <alex.otieno@simpleformations.ke>
2023-06-19 21:23:35 +0300

    Sign In, Sign Up Modal Controller Configuration

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,24 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
-import { enter,leave } from "el-transition";
+import { enter, leave } from "el-transition";
 
 export default class extends Controller {
   connect() {
-    enter(document.getElementById("modal-wrapper"));
-    enter(document.getElementById("modal-backdrop"));
-    enter(document.getElementById("modal-panel"));
-
     document
       .getElementById("modal-wrapper")
       .addEventListener("click", this.closeModal);
   }
 
   closeModal(event) {
-    const modalClicked = document.getElementById("modal-panel").contains(event.target);
+    const modalPanelClicked = document
+      .getElementById("modal-panel")
+      .contains(event.target);
 
-    if (!modalClicked) {
+    if (!modalPanelClicked && event.target.id !== "modal-trigger") {
       leave(document.getElementById("modal-wrapper"));
       leave(document.getElementById("modal-backdrop"));
       leave(document.getElementById("modal-panel"));
     }
+  }
+
+  showModal() {
+    enter(document.getElementById("modal-wrapper"));
+    enter(document.getElementById("modal-backdrop"));
+    enter(document.getElementById("modal-panel"));
   }
 }
