afd20e2c59818fc870afb0c6a9ac5cf4c34ab16f
AlexCosmas <alex.otieno@simpleformations.ke>
2023-06-19 15:56:48 +0300

    Added the leave modal functionality on the modal controller.js partial

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,13 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
-import { enter } from "el-transition";
+import { enter,leave } from "el-transition";
 
 export default class extends Controller {
   connect() {
-//    console.log('Hello, We are inside the modal controller')
-   enter(document.getElementById('modal-wrapper'));
-   enter(document.getElementById('modal-backdrop'));
-   enter(document.getElementById('modal-panel'));
+    enter(document.getElementById("modal-wrapper"));
+    enter(document.getElementById("modal-backdrop"));
+    enter(document.getElementById("modal-panel"));
+
+    document
+      .getElementById("modal-wrapper")
+      .addEventListener("click", this.closeModal);
   }
 
+  closeModal(event) {
+    const modalClicked = document.getElementById("modal-panel").contains(event.target);
 
+    if (!modalClicked) {
+      leave(document.getElementById("modal-wrapper"));
+      leave(document.getElementById("modal-backdrop"));
+      leave(document.getElementById("modal-panel"));
+    }
+  }
 }
