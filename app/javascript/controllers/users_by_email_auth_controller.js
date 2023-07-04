d8a0c03b31a7d06e16f5d701138d23b7e6540e3a
Alex Cosmas <cosmasalex365@gmail.com>
2023-07-04 21:58:41 +0300

    Fix: User by Email Controller

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,11 +1,20 @@
-import { Controller } from "@hotwired/stimulus";
+import {Controller} from "@hotwired/stimulus";
 
 
 export default class extends Controller {
 
- static targets = [ 'email', 'submit']
- connect() {
-     console.log('emailTarget:', this.emailTarget)
-     console.log('submitTarget:', this.submitTarget)
- }
+    static targets = ['email', 'submit']
+
+    connect() {
+        this.submitTarget.addEventListener('click', (e) => {
+                e.preventDefault();
+                if (this.emailTarget.value.length === 0) {
+                    //email is empty
+                    console.log('Show this if email field is empty',)
+                } else {
+                    //email is filled, do something
+                }
+            }
+        )
+    }
 }
