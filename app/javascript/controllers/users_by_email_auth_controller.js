e0770b8491604fb0e49bd69864836e7bc7ca8e87
Alex Cosmas <cosmasalex365@gmail.com>
2023-07-09 22:16:23 +0300

    Submit user auth for in modal - Axios(Fix)

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,20 +1,37 @@
-import {Controller} from "@hotwired/stimulus";
-
+import { Controller } from "@hotwired/stimulus"
+import axios from 'axios'
 
 export default class extends Controller {
 
     static targets = ['email', 'submit']
 
     connect() {
+
+        console.log('Controller connected')
+
+        // console.log('This is the email target', this.emailTarget)
+        // console.log('This is the submit target', this.submitTarget)
+
         this.submitTarget.addEventListener('click', (e) => {
-                e.preventDefault();
-                if (this.emailTarget.value.length === 0) {
-                    //email is empty
-                    console.log('Show this if email field is empty',)
-                } else {
-                    //email is filled, do something
-                }
+            e.preventDefault();
+            if (this.emailTarget.value.length === 0) {
+                //email is empty
+                console.log('Show this if email field is empty',)
+            } else {
+                //email is filled, do something
+                axios.get('/api/users_by_email', {
+                    params: {
+                        email: this.emailTarget.value
+                    },
+                    headers: {
+                        "ACCEPT" : "application/json"
+                    }
+                }).then((response) => {
+                    Turbo.visit('/users/sign_in')
+                }).catch(function (error) {
+                    Turbo.visit('/users/sign_up')
+                })
             }
-        )
+        })
     }
 }
