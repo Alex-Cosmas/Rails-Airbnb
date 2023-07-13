21d936ec06abe3685f136e0b5a69a3a9caf2faf9
Alex Cosmas <cosmasalex365@gmail.com>
2023-07-13 16:14:55 +0300

    Fix axios

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -8,6 +8,7 @@ export default class extends Controller {
     connect() {
 
         console.log('Controller connected')
+        console.log('axios connected successfully:', axios)
 
         // console.log('This is the email target', this.emailTarget)
         // console.log('This is the submit target', this.submitTarget)
@@ -19,18 +20,19 @@ export default class extends Controller {
                 console.log('Show this if email field is empty',)
             } else {
                 //email is filled, do something
-                axios.get('/api/users_by_email', {
-                    params: {
-                        email: this.emailTarget.value
-                    },
-                    headers: {
-                        "ACCEPT" : "application/json"
-                    }
-                }).then((response) => {
-                    Turbo.visit('/users/sign_in')
-                }).catch(function (error) {
-                    Turbo.visit('/users/sign_up')
-                })
+                console.log('do something')
+                // axios.get('/api/users_by_email', {
+                //     params: {
+                //         email: this.emailTarget.value
+                //     },
+                //     headers: {
+                //         "ACCEPT" : "application/json"
+                //     }
+                // }).then((response) => {
+                //     Turbo.visit('/users/sign_in')
+                // }).catch(function (error) {
+                //     Turbo.visit('/users/sign_up')
+                // })
             }
         })
     }
